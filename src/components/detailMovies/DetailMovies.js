e0c65b69a526bfa49cb89559de33f1a2c8324edc
Alejandro-va <prof.jahz@gmail.com>
2023-05-31 20:48:00 -0300

    details

--- src/components/detailMovies/DetailMovies.js
+++ src/components/detailMovies/DetailMovies.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const DetailMovies = () => {
   const { id } = useParams();
-  console.log(id);
+  //console.log(id);
+  const movie = useSelector((state) => state.storeMovie.stateMovies);
+  const detailMovie = movie.find((item) => item.imdbID === id);
+  console.log(detailMovie);
+
   return (
     <div>
       DetailMovies
-      <span>{id}</span>
+      <span>--{id}</span>
+      <img src={detailMovie.Poster} alt="" />
+      <h1>{detailMovie.Title}</h1>
     </div>
   );
 };

e0010c853695409bf0fd1e1d7e4a7787136709ed
ManuMazzino <tucorreo>
2023-06-06 02:15:23 -0300

    carousel de peliculas con libreria (en progreso)

--- src/components/listMovies/ListMovies.js
+++ src/components/listMovies/ListMovies.js
@@ -3,27 +3,38 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 //REDUX
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovies } from "../../feactures/movies/moviesSlice";
+
+import { Carousel } from '@trendyol-js/react-carousel';
+
+
 
 const ListMovies = () => {
   
-  const selectorMovie = useSelector((state) => state.storeMovie.stateMovies);
-
- 
   
+  const selectorMovie = useSelector((state) => state.storeMovie.stateMovies);
+  console.log(selectorMovie)
+   
 
   return (
     <div>
       <h1>LisMovies</h1>
-      {selectorMovie.map((el) => (
-        <ul>
-          <li>
-            <Link to={`/detailMovies/${el.id}`}> {el.title}</Link>
-          </li>
-        </ul>
-      ))}
+      <Carousel show={3.5} slide={3} swiping={true}>
+        {selectorMovie.map((el) => (
+          <div key={el.id}>
+            <ul>
+            <li>
+              <Link to={`/detailMovies/${el.id}`}> {el.title}</Link>
+              <img src={"https://image.tmdb.org/t/p/w500/" + el.poster_path} alt="" />
+              
+            </li>
+          </ul>
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
 
 export default ListMovies;
+
+

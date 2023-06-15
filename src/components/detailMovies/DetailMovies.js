e59b16712d6ffa419dbada4e098dbc2ec1932500
ManuMazzino <tucorreo>
2023-06-15 15:23:03 -0300

    trae el trailer de la peli de youtube

--- src/components/detailMovies/DetailMovies.js
+++ src/components/detailMovies/DetailMovies.js
@@ -11,34 +11,38 @@ const DetailMovies = () => {
   const detailMovie = movies.find((item) => item.id == id);
   console.log(movies);
   const [videos, setVideos] = useState([]);
-  // const videoKey= {`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}append_to_response=videos`
-
-  
+   
   useEffect (() =>{
     (movieApi.get(`3/movie/${id}/videos`))
     .then(response => setVideos(response.data.results))
   }, [id]) 
 
   if (!detailMovie) return <div>carganding...</div>;
+
   
-  // Youtube: https://www.youtube.com/watch?v=hzfvYSIIgyc
-  // Vimeo: https://vimeo.com/282875052
+  const trailer=videos.find(video=> video.type=="Trailer")
+
 
 
   return (
     <div>
       DetailMovies
-      <span>--{id}</span>
-      <img src={"https://image.tmdb.org/t/p/w500/" +  detailMovie.poster_path} alt="" />
-      <div>
-        {videos.map(video => 
-          // video.key  
-        <a href={"https://www.youtube.com/watch?v=" + video.key}> ir</a>
-        ) }
-      {/* {`https://api.themoviedb.org/3/movie/`+ detailMovie.video} */}
-      eN7eA3_Hn_g
-      </div>
       <h1>{detailMovie.title}</h1>
+      <span>--{id}</span>
+      <img src={"https://image.tmdb.org/t/p/w500/" +  detailMovie.poster_path } height={"200px"} alt="" />
+      <div>
+        {/* 
+        quizas podemos poner botones o tipo playlist con todos los demas videos...
+                
+        {videos.map(video => 
+        // <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + video.key} title="YouTube video player" frameborder="0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        ) } */}
+        {
+          trailer ?  <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + trailer.key} title="YouTube video player" frameborder="0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> : undefined
+        }
+  
+
+      </div>
     </div>
   );
 };

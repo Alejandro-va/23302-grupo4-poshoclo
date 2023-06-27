62ff3fc2714e2b3b923c805d54c3c7203b98820f
ManuMazzino <tucorreo>
2023-06-27 01:04:55 -0300

    portada provisoria. Deje boton con link a pelis hasta que armemos registro y nav

---
+++ src/components/portada.js
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+
+export default function Portada() {
+    
+    return(
+        <div className='portada'>
+            <div className='logoPortada'>
+                <img src="/logo1.PNG" alt=""/>
+                <h2>Las películas que querés, donde las necesitas.</h2>
+                <button> <Link to={`/Peliculas`} className="link">provisorio a pelis</Link></button>
+            </div>
+            <div className='fondoPortada'>
+                <img src="/fondo inicio.png" alt=""/>
+            </div>
+        </div>
+    )
+}
+

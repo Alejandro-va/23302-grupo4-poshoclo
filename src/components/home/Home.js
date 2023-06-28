e01dbf05726957a16f3de409f85a6afd3c55595a
lucianadm <lucianadm55@hotmail.com>
2023-06-27 22:17:31 -0300

    agregue navbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,10 +3,13 @@ import ListMovies from "../listMovies/ListMovies";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailMovies from "../detailMovies/DetailMovies";
 import Portada from "../portada";
+import NavbarPelis from "../navbarPelis";
+
 
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies } from "../../feactures/movies/moviesSlice";
 import { fetchGenres } from "../../feactures/genres/genresSlice";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -19,9 +22,11 @@ const Home = () => {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/peliculas" element={<ListMovies />} />
-          <Route path="/detailMovies/:id" element={<DetailMovies />} />
-          <Route path="/" element={<Portada/>} />
+        
+          <Route path="/peliculas" element={<div><NavbarPelis /><ListMovies /></div>} />
+          <Route path="/detailMovies/:id" element={<div><NavbarPelis /><DetailMovies /></div>} />
+ 
+          <Route path="/" element={<div><NavbarPelis /><Portada /></div>} />
         </Routes>
       </BrowserRouter>
     </div>

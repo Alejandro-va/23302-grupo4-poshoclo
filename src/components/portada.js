bd73c1df4fe68b45ab57a35ffe7551ab2959f4ff
Alejandro-va <prof.jahz@gmail.com>
2023-07-07 10:57:20 -0300

    portada

--- src/components/portada.js
+++ src/components/portada.js
@@ -2,12 +2,24 @@ import { Link } from "react-router-dom";
 
 export default function Portada() {
   return (
-    <div className="portada">
+    <div className="portada" style={{ overflow: "hidden", height: "88vh" }}>
       <div className="logoPortada">
-        <img src="/logo1.PNG" alt="" />
+        <img
+          src="/logo1.PNG"
+          alt=""
+          style={{ objectFit: "fill", marginTop: "5rem" }}
+        />
         <h2>Las películas que querés, donde las necesitas.</h2>
         {/* <button> <Link to={`/Peliculas`} className="link">provisorio a pelis</Link></button> */}
-        <button>
+        <button
+          style={{
+            marginTop: "1rem",
+            padding: ".5rem 3rem",
+            borderRadius: "5%",
+            outline: ".3px solid #fff",
+            fontWeight: "bold",
+          }}
+        >
           {" "}
           <Link to={`/login`} className="link">
             Login
